fe64517811c78c3d3ea436592802bd8d06b84773
rahul kumar gupta <73681689+rahulgupta16102000@users.noreply.github.com>
2022-11-19 16:06:05 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -78,7 +78,8 @@ const Header = () => {
         </a>
         <Link to={"/cart"}>
           <FiShoppingBag />
-          <p>{cartItems.length}</p>
+          <p style={{    fontSize: large;
+    transform: translateY(-10px);}}>{cartItems.length}</p>
         </Link>
       </div>
     </nav>
